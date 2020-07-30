88a47fa7cbad2f2829231996ffa67d65d1b33ade
parkermax93 <parker.williamson@gmail.com>
2020-07-30 14:33:57 -0700

    fix log out and layout

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,7 @@ Rails.application.routes.draw do
 
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
-  get '/logout' => 'sessions#destroy'
-  post '/logout' => 'sessions#destroy'
+  delete '/logout' => 'sessions#destroy'
 
   resources :users, only: [:new, :create, :show]
   resources :comments
