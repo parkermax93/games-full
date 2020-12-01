0a1d711b8dd1087ced1c8c0195d52138cf6f9121
parkermax93 <parker.williamson@gmail.com>
2020-11-30 17:14:26 -0800

    remove commented code, bold game name

--- app/models/game.rb
+++ app/models/game.rb
@@ -7,16 +7,12 @@ class Game < ApplicationRecord
     validates :name, :developer, :description, presence: true
     validates :release_year, numericality: {only_integer: true}
 
-    # def user_attributes=(user_attributes)
-    #     self.user = User.find_or_create_by(username: user_attributes[username]) unless user_attributes[:username].blank?
-    # end
 
     def self.org
         self.order(:name)
     end
 
     def self.most_popular
-        # self.left_joins(:comments).group(:id).order('COUNT(comments.id) DESC')
         self.all.sort_by do |game|
             game.comments.length
         end.reverse 
